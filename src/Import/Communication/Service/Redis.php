acd0b4157ef92d4fd23b6c2951c97767fc626a0b
Rafal Wesolowski <wesolowski@nexus-netsoft.com>
2019-04-12 12:45:51 +0200

    change travis

--- src/Import/Communication/Service/Redis.php
+++ src/Import/Communication/Service/Redis.php
@@ -14,13 +14,14 @@ class Redis
     public function __construct()
     {
         $this->redis = new \Redis;
-        $this->redis->connect('redis');
+        $this->redis->connect('redis', 6379);
         $this->redis->setOption(\Redis::OPT_PREFIX, 'price:');
     }
 
     public function set($key, $value)
     {
         $this->cache[$key] = $value;
+        $this->i++;
 
         if($this->i > 500) {
             $this->redis->mset($this->cache);
